8362ec9260081d7fe98ee5faadb38f2f6edbb9a3
Anhle65 <levananh235813@gmail.com>
2025-01-01 15:50:24 +1300

    Refined code

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace shopping_api.Controllers;
 using shopping_api.Models;
@@ -10,11 +11,7 @@ using System.ComponentModel;
 [Route("products")]
 public class ProductController : ControllerBase
 {
-
-    [HttpGet]
-    public ActionResult<List<Product>> GetAll()
-    {
-        return new List<Product> {
+    private List<Product> allProducts = new List<Product> {
             new Product 
                 {   Id = 1, 
                     Name = "Apple", 
@@ -29,25 +26,15 @@ public class ProductController : ControllerBase
                     Description = "Constant consume"
                 }
             };
+    [HttpGet]
+    public ActionResult<List<Product>> GetAll()
+    {
+        return allProducts;
     }
 
     [HttpGet("{id}")]
     public ActionResult<Product> Get(int id)
     {
-        var allProducts = new List<Product> {
-                            new Product {   
-                                Id = 1,
-                                Name = "Apple",
-                                Price = 1.5f,
-                                Description = "Constant consume"
-                            },
-                            new Product {
-                                Id = 2,
-                                Name = "Cherry",
-                                Price = 5.5f,
-                                Description = "Constant consume"
-                            }
-                            };
         var product = allProducts.FirstOrDefault(p=> p.Id == id);
         if(product == null)
         {
